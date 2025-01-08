85cde55b81a10188aa88cef4cb2c674dd5f9ffcd
Vidura Priyadarshana <vidura200427@gmail.com>
2025-01-08 16:19:18 +0530

    entity added

---
+++ server/tasks/src/main/java/com/vidura/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.vidura.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH,MEDIUM,LOW
+}
---
+++ server/tasks/src/main/java/com/vidura/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.vidura.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN,CLOSED
+}
