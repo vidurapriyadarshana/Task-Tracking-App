e8bb917ba91ee7b700f4ab8107894ccf4a3eee02
Vidura Priyadarshana <vidura200427@gmail.com>
2025-02-10 14:33:34 +0530

    Added Repository

---
+++ server/tasks/src/main/java/com/vidura/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,12 @@
+package com.vidura.tasks.repositories;
+
+
+import com.vidura.tasks.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
